e3f5110adcd93e9279f675ee4db5725ab3e60e20
TranVanThang510003 <thangtran00030@gmail.com>
2024-06-01 16:34:39 +0700

    bai 63 layout mac dinh

--- src/components/Layout/DefaultLayout/index.js
+++ src/components/Layout/DefaultLayout/index.js
@@ -1,14 +1,18 @@
+
+import classNames from 'classnames/bind';
 import Header from '~/components/Layout/components/Header';
+import styles from './DefaultLayout.module.scss'
 import SideBar from "./SideBar";
 
+const cx= classNames.bind(styles)
 function DefaultLayout({children}){
     return (
 
-      <div>
+      <div className={cx('wraper')}>
             <Header/>
-            <div className="container">
+            <div className={cx('container')}>
                 <SideBar/>
-                <div className="content">
+                <div className={cx('content"')}>
                     {children}
                 </div>
     
